c500843bfebe7c9ce3eebacf255a3b8f2e7f2ab6
NatanBagrov <natanzb@gmail.com>
2018-12-11 19:55:52 +0200

    normalized

--- HW1/forward_inverse_problem.py
+++ HW1/forward_inverse_problem.py
@@ -13,11 +13,11 @@ def forward_blur(original_image, psfs):
 
 def inverse_deblur(blurred_images):
     print("Generating a de-blurred image using {} blurred images".format(len(blurred_images)))
-    blurred_ffts = np.array([fft2(im) for im in blurred_images])
+    normalized_blurred_images = np.array(blurred_images) / 255.0
+    blurred_ffts = fftpack.fftshift(fftpack.fftn(normalized_blurred_images))
     abs_ffts = np.abs(blurred_ffts)
     argmax_ffts = np.argmax(abs_ffts, axis=0)
     max_fft = np.zeros(blurred_ffts.shape[1:], dtype=complex)
     for i in range(argmax_ffts.shape[0]):  # Sorry, didn't want to spend time optimizing this damn for-for-loops
         for j in range(argmax_ffts.shape[1]):
             max_fft[i, j] = blurred_ffts[argmax_ffts[i, j], i, j]
-
